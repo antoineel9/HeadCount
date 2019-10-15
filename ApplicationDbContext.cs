172bd2ccf6bdf5db55d8bb743569e888bbf2bb01
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-15 12:43:11 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HeadCount.Data;
+
+namespace HeadCount.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HeadCount.Data.Department> Department { get; set; }
+        public DbSet<HeadCount.Data.Employee> Employee { get; set; }
+    }
+}
